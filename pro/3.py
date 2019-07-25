ddd48ef1e70381c8ae0cfca1de6ba62f432b8831
bharathindhumati <51354592+bharathindhumati@users.noreply.github.com>
2019-07-25 16:04:30 +0530

    Update 3.py

--- pro/3.py
+++ pro/3.py
@@ -1,8 +1,8 @@
- m,n=input().split()
+m,n=input().split()
 o=abs(len(n)-len(m))
 for i in range(len(m)):
-	    if(len(n)==1 and n[i] in m):
-	    	    break
-	    if(m[i]!=n[i]):
-	    	    o=o+1
+    if(len(n)==1 and n[i] in m):
+        break
+    if(m[i]!=n[i]):
+        o=o+1
 print(o)
